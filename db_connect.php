1d823e5c8e907279d262bb3433b8afd7192b467b
Evan Lee <75407120+EvanLee25@users.noreply.github.com>
2022-12-16 01:08:25 -0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -2,8 +2,8 @@
 $host = "localhost";
 $username = "root";
 $user_pass = "root";
-//$database_in_use = "jokes_table";
-$database_in_use = "bankingdemo";
+$database_in_use = "jokes_table";
+//$database_in_use = "bankingdemo";
 
 $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
 
